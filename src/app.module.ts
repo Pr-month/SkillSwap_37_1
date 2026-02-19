652c9ab1f3fed811c4f0c22963f3f642b65ba6cb
Kirin <kirin-89@yandex.ru>
2026-02-19 12:45:12 +1000

    feat(db): настроить подключение PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { appConfig } from './config/app.config';
+import { AppDataSource } from './config/ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+    TypeOrmModule.forRoot(AppDataSource.options),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/ormconfig.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USERNAME ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? 'postgres',
+  database: process.env.DB_NAME ?? 'skillswap',
+
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+
+  synchronize: (process.env.TYPEORM_SYNCHRONIZE ?? 'true') === 'true',
+  logging: (process.env.TYPEORM_LOGGING ?? 'false') === 'true',
+});
