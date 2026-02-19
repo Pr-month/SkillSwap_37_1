b13d2634f3055a7011e03072c1e2028eb4c873cc
Warrain <Arain1@rambler.ru>
2026-02-19 22:39:09 +0000

    connection ConfigModule in app.module.ts 1.1 xWarrainx

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { appConfig } from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [appConfig],
     }),
     AuthModule,
     UsersModule,
