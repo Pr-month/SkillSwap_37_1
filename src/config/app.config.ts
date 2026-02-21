8d8d29d546cfaccf98a76aa07797ff4261632250
Aleksandr Drozdov <d-a-s91@yandex.ru>
2026-02-21 14:20:30 +0300

    feat: add app_config hashSalt

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,8 @@ import { registerAs } from '@nestjs/config';
 import { ConfigType } from '@nestjs/config';
 
 export const appConfig = registerAs('APP_CONFIG', () => ({
-  port: Number(process.env.PORT ?? 3000),
+  port: Number(process.env.PORT ?? 10),
+  hashSalt: Number(process.env.HASH_SALT ?? 10),
 }));
 
 export type AppConfig = ConfigType<typeof appConfig>;
