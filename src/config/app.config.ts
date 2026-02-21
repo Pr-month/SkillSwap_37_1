a55471a74acb0291df1599c42d4630fbdc21d6a6
Aleksandr Drozdov <d-a-s91@yandex.ru>
2026-02-21 16:04:10 +0300

    fix: config port

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { ConfigType } from '@nestjs/config';
 
 export const appConfig = registerAs('APP_CONFIG', () => ({
-  port: Number(process.env.PORT ?? 10),
+  port: Number(process.env.PORT ?? 3000),
   hashSalt: Number(process.env.HASH_SALT ?? 10),
 }));
 
