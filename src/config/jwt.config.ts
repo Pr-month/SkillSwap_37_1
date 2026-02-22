7231b5c6b0580253c923b84c054cb7c60b437d68
Warrain <Arain1@rambler.ru>
2026-02-22 15:35:12 +0000

    configure JWT add StringValue xwarrainx

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,13 +1,12 @@
 import { registerAs, ConfigType } from '@nestjs/config';
+import { StringValue } from '@nestjs/jwt';
 
-export const jwtConfig = registerAs('JWT_CONFIG', () => {
-  return {
-    secret: process.env.JWT_SECRET ?? 'default-secret-change-me',
-    refreshSecret:
-      process.env.JWT_REFRESH_SECRET ?? 'default-refresh-secret-change-me',
-    expiresIn: process.env.JWT_EXPIRES_IN ?? '15m',
-    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? '7d',
-  };
-});
+export const jwtConfig = registerAs('JWT_CONFIG', () => ({
+  secret: process.env.JWT_SECRET ?? 'default-secret-change-me',
+  refreshSecret:
+    process.env.JWT_REFRESH_SECRET ?? 'default-refresh-secret-change-me',
+  expiresIn: process.env.JWT_EXPIRES_IN ?? ('15m' as StringValue),
+  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? ('7d' as StringValue),
+}));
 
 export type TJwtConfig = ConfigType<typeof jwtConfig>;
