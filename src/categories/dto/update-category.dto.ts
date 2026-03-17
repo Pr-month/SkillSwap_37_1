3a1a3df7e407cf89d16c6be24f7311a373cb4492
Warrain <Arain1@rambler.ru>
2026-03-17 15:09:45 +0000

    bug change swagger xWarrainx

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,10 +1,20 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class UpdateCategoryDto {
+  @ApiPropertyOptional({
+    example: 'IT и программирование (обновлено)',
+    description: 'Новое название категории',
+  })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiPropertyOptional({
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    description: 'ID новой родительской категории',
+    nullable: true,
+  })
   @IsOptional()
   @IsUUID()
   parentId?: string;
