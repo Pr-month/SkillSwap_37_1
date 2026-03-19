b24d704ceb3d58ce915c1fc78f17e431c73283d5
Warrain <Arain1@rambler.ru>
2026-03-19 20:25:07 +0000

    swagger controller auth xWarrainx

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,18 +14,26 @@ import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { RefreshAuthGuard } from './guards/refresh-auth.guard';
 import { AuthRequest, RefreshRequest } from './types/auth.types';
+import {
+  ApiRegister,
+  ApiLogin,
+  ApiLogout,
+  ApiRefresh,
+} from './swagger/auth.swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiRegister()
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @ApiLogin()
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
@@ -33,12 +41,14 @@ export class AuthController {
   @Post('logout')
   @UseGuards(JwtAuthGuard)
   @HttpCode(HttpStatus.OK)
+  @ApiLogout()
   logout(@Req() req: AuthRequest) {
     return this.authService.logout(req.user.sub);
   }
 
   @Post('refresh')
   @UseGuards(RefreshAuthGuard)
+  @ApiRefresh()
   async refresh(@Request() req: RefreshRequest) {
     return this.authService.refreshTokens(req.user.sub, req.user.refreshToken);
   }
