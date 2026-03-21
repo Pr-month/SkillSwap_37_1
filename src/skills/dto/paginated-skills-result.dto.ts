3fa21dd5d037f1a8b1786b9c2c097911d8d114e4
Warrain <Arain1@rambler.ru>
2026-03-21 23:31:19 +0000

    swagger controller skills xWarrainx

--- src/skills/dto/paginated-skills-result.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Skill } from '../entities/skill.entity';
-import { Type } from 'class-transformer';
-
-export class PaginatedSkillsResultDto {
-  data: Skill[];
-  page: number;
-  limit: number;
-  totalCount: number;
-}
