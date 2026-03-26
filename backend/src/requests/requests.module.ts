914412834a749b194f2aff319475a7752eb9dff4
Warrain <Arain1@rambler.ru>
2026-03-26 23:42:19 +0000

    add email service xWarrainx

--- backend/src/requests/requests.module.ts
+++ backend/src/requests/requests.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { RequestsService } from './requests.service';
 import { RequestsController } from './requests.controller';
 import { Request } from './entities/request.entity';
@@ -10,6 +11,10 @@ import { NotificationModule } from 'src/notification/notification.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Request, Skill, User]),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
     NotificationModule,
   ],
   controllers: [RequestsController],
